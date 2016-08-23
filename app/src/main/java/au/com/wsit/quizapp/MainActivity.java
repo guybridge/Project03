fc3b57302d8d966d30ced7a1f0cc819c10dc265a
Guy Bridge <guy@wsit.com.au>
2016-08-23 16:33:24 +0800

    Started creating layout

--- app/src/main/java/au/com/wsit/quizapp/MainActivity.java
+++ app/src/main/java/au/com/wsit/quizapp/MainActivity.java
@@ -2,9 +2,16 @@ package au.com.wsit.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private TextView mFirstNumber;
+    private TextView mSecondNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,5 +19,40 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirstNumber = (TextView) findViewById(R.id.firstNumberTextView);
+        mSecondNumber = (TextView) findViewById(R.id.secondNumberTextView);
+
+        // Generate a question
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        int id = item.getItemId();
+
+        switch (id)
+        {
+            case R.id.settings:
+                // Start settings
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/au/com/wsit/quizapp/Question.java
+++ app/src/main/java/au/com/wsit/quizapp/Question.java
@@ -5,5 +5,37 @@ package au.com.wsit.quizapp;
  */
 public class Question
 {
+    private String mSkillLevel;
+    private int mFirstNumber;
+    private int mSecondNumber;
+
+    public Question(String skillLevel)
+    {
+        mSkillLevel = skillLevel;
+
+        if (mSkillLevel.equals("EASY"))
+        {
+            // Set number range
+
+        }
+        else if(mSkillLevel.equals("MEDIUM"))
+        {
+          // Set number range
+        }
+        else if(mSkillLevel.equals("HARD"))
+        {
+            // Set number range
+        }
+    }
+
+    public int[] create()
+    {
+        mFirstNumber = 10;
+
+        return null;
+
+
+    }
+
 
 }
