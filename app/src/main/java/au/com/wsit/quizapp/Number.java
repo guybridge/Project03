850f646b109db6d986481f73a95da4a324da7b09
Guy Bridge <guy@wsit.com.au>
2016-08-23 15:33:45 +0800

    Added Question and Number class

---
+++ app/src/main/java/au/com/wsit/quizapp/Number.java
@@ -0,0 +1,27 @@
+package au.com.wsit.quizapp;
+
+import java.util.Random;
+
+/**
+ * Class which generates a random number
+ */
+public class Number
+{
+    // Member variables
+    private int mRandomNumberMax;
+
+    // Constructor
+    public Number(int numberMax)
+    {
+        mRandomNumberMax = numberMax;
+    }
+
+    public int generate()
+    {
+        Random random = new Random();
+        int number = random.nextInt(mRandomNumberMax);
+
+        return number;
+
+    }
+}
---
+++ app/src/main/java/au/com/wsit/quizapp/Question.java
@@ -0,0 +1,9 @@
+package au.com.wsit.quizapp;
+
+/**
+ * Created by guyb on 23/08/16.
+ */
+public class Question
+{
+
+}
